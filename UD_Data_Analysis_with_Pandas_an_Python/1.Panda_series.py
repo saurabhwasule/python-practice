0a011d995c900e6af3b73ea29a2b7eef33818de2
Saurabh <saurabhwasule5@gmail.com>
2018-02-19 09:43:51 +0530

    panda day 2 few more methods

--- UD_Data_Analysis_with_Pandas_an_Python/1.Panda_series.py
+++ UD_Data_Analysis_with_Pandas_an_Python/1.Panda_series.py
@@ -34,21 +34,49 @@ print(pd.Series(data=weekdays,index=num1))
 
 # read_csv Method
 
-s=pd.read_csv("pandas//pokemon.csv",usecols=["Pokemon"],squeeze=True) #  squeeze parameter's argument to import the data as a Series object instead of a DataFrame.
-s1=pd.read_csv("pandas//google_stock_price.csv",squeeze=True)
+pokemon=pd.read_csv("practice_csv//pokemon.csv",usecols=["Pokemon"],squeeze=True) #  squeeze parameter's argument to import the data as a Series object instead of a DataFrame.
+pokemon1=pd.read_csv("practice_csv//pokemon.csv",index_col="Pokemon",squeeze=True)
+google=pd.read_csv("practice_csv//google_stock_price.csv",squeeze=True)
 
 # Head and tail Method
-pd.Series(s.head(n=10)) # Create a new series with 10 element
-s_google_tail5=pd.Series(s1.tail()) # Create a new series with default 5 element
+pd.Series(pokemon.head(n=10)) # Create a new series with 10 element
+s_google_tail5=pd.Series(google.tail()) # Create a new series with default 5 element
 print(s_google_tail5)
 
 # Python Built-In Functions
 
-print(len(s1))
-print(type(s1))
+print(len(google))
+print(type(google))
 print(list(s_google_tail5)) # convert series to list
 print(dict(s_google_tail5)) # convert series to dictionary
-print(min(s))
-print(max(s1))
+print(min(pokemon))
+print(max(google))
 
-#
+# Sort_value method
+
+pd.Series(s_google_tail5).sort_values(ascending=False) # Returns new series in ascending order
+google.sort_values(ascending=True,inplace=True)  # place parameter on a Series method to permanently modify the object it is called on
+# or
+google=google.sort_values(ascending=False) # or we cane reassigning the new object to the same variable to permanently modify the object it is called on
+
+# Sort_index method
+
+google.sort_index() # sort by index and if we again sort the google variable by index it will go to it's original state
+google.sort_index(ascending=False,inplace=True) # overwrite  the object and stored desc order series
+
+# in keyword
+# to check if a value exists in either the values or index of a Series.
+# If the .index or .values attribute is not included, pandas will default to searching among the Series index.
+
+print("Pikachu" in pokemon.values) # return true if the element exits in series otherwise false
+
+print(100 in pokemon.index) # return true if the index  exits in series otherwise false
+
+# Extract Series Values by Index Position
+# Use bracket notation to extract Series values by their index position.
+
+print(pokemon[[200,1,14]])  # return series element from 200,1,14 index location
+print(pokemon[10:20])  # return  element from 10 to 20
+print(pokemon[:10])  # return element till 10
+print(pokemon[-10:])  # return  element 10 element from last
+
